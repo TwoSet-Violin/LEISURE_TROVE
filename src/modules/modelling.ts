ddc4d3a4b057dc65923bfd95095c9c42ba96b948
TwoSet-Violin <95968211+TwoSet-Violin@users.noreply.github.com>
2023-03-23 22:26:18 +0000

    bump version to 2.0.1

--- src/modules/modelling.ts
+++ src/modules/modelling.ts
@@ -35,3 +35,50 @@ async function getData(): Promise<Array<InputData>> {
 }
 
 function createModel(): tf.Sequential {
+  // Create a sequential model
+  const blankModel = tf.sequential();
+
+  // Add a single input layer
+  blankModel.add(tf.layers.dense({
+    inputShape: [1], units: 16, useBias: true, activation: 'relu',
+  }));
+  blankModel.add(tf.layers.dense({ units: 8, activation: 'relu' }));
+
+  // Add an output layer
+  blankModel.add(tf.layers.dense({ units: 1, useBias: true }));
+
+  return blankModel;
+}
+
+/**
+ * Convert the input data to tensors that we can use for machine
+ * learning. We will also do the important best practices of _shuffling_
+ * the data and _normalizing_ the data
+ * MPG on the y-axis.
+ */
+function convertToTensor(data: InputData[]): ConvertedInputs {
+  // Wrapping these calculations in a tidy will dispose any
+  // intermediate tensors.
+
+  return tf.tidy(() => {
+    // Step 1. Shuffle the data
+    tf.util.shuffle(data);
+
+    // Step 2. Convert data to Tensor
+    const inputs = data.map((d) => d.horsepower);
+    const labels = data.map((d) => d.mpg);
+
+    const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+    const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+    // Step 3. Normalize the data to the range 0 - 1 using min-max scaling
+    const inputMax = inputTensor.max();
+    const inputMin = inputTensor.min();
+    const labelMax = labelTensor.max();
+    const labelMin = labelTensor.min();
+
+    const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+    const normalizedLabels = labelTensor.sub(labelMin).div(labelMax.sub(labelMin));
+
+    return {
+      inputs: normalizedInputs,
